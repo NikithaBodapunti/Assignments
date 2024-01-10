5f05367e7ab8d60cb6ce68126a4d16e757fbc196
Nikitha <nikitha@Nikithas-MBP.lan>
2024-01-09 18:35:57 -0600

    commit assignments

---
+++ Day 2 Temp.py
@@ -0,0 +1,15 @@
+#Temperature Converter
+
+c = float(input("enter temperature in celcius "))
+
+f = float((c*1.8)+32)
+
+print(f)
+
+f = float(input("enter temperature in fahrenheit "))
+
+c = float((f-32)*(5/9))
+
+print(c)
+
+
---
+++ Day 2.py
@@ -0,0 +1,21 @@
+#Arithmetic Operations
+
+x = int(input("Enter a number "))
+
+y = int(input("enter a number "))
+
+sum = x+y
+
+print("sum is " + str(sum))
+
+subraction = x-y
+
+print("subraction is " + str(subraction))
+
+multiplication = x*y
+
+print("multiplication is " + str(multiplication))
+
+division = x/y
+
+print("division is " + str(division))
